388134db27365a647446c930b86ed56c6ff5d45d
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-10-12 19:23:43 -0700

    Updated Notes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,13 +27,34 @@ namespace Prog_122_L8_RTB_Lists
 
         private void btnDisplayText_Click(object sender, RoutedEventArgs e)
         {
-            string usersAnswer = rtbDisplay.Text;
 
-            MessageBox.Show(usersAnswer);
-
+            AccidentInfo();
 
         } // btnDisplayText_Click
-        
+
+        public void AccidentInfo()
+        {
+            string driver = txtDriverName.Text;
+            string address = txtAddress.Text;
+            string make = txtMake.Text;
+            string model = txtModel.Text;
+            string year = txtYear.Text;
+            string info = rtbAccidentInfo.Text;
+
+            //string formattedInformation = $"{driver} {address} {make} {model} {year} {info}";
+
+            rtbDisplay.Text = $"Driver Name: {driver}\n";
+            rtbDisplay.Text += $"Address : {address}";
+            rtbDisplay.Text += $"Make : {make}";
+            rtbDisplay.Text += $"Model : {model}";
+            rtbDisplay.Text += $"Year : {year}";
+            rtbDisplay.Text += $"Info : {info}";
+
+
+
+
+        } // AccidentInfo
+
     } // class
 
 } // namespace
